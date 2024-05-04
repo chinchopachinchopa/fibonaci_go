1e09bafafb4776ad93ae5362b774c8f367937c1d
Jambo2 <egor.zaytsev.2018@list.ru>
2024-05-04 08:46:04 +0300

    structure update

--- main.go
+++ main.go
@@ -1,58 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"net/http"
+	"main/ui/console"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>piska</h1>"))
-}
-
-func isPerfectSquare(n int) bool {
-	sq := int(math.Sqrt(float64(n)))
-	return sq*sq == n
-}
-
-func isFibonacci(n int) bool {
-	return isPerfectSquare(5*n*n+4) || isPerfectSquare(5*n*n-4)
-}
-
-func fibonacci(n int) (int, int) {
-	prev := 0
-	curr := 1
-	for curr < n {
-		prev, curr = curr, prev+curr
-	}
-	return prev, curr
-}
-
 func main() {
-	port := "8080"
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", indexHandler)
-
-	http.ListenAndServe(":"+port, mux)
-	var num int
-	fmt.Print("Введите число: ")
-	_, err := fmt.Scan(&num)
-	if err != nil {
-		fmt.Println("Ошибка.Введите число.")
-		return
-	}
-
-	if isFibonacci(num) {
-		prev, curr := fibonacci(num)
-		fmt.Printf("Числа фибоначи рядом: %d, %d\n", prev, curr+prev)
-	} else {
-		prev, curr := fibonacci(num)
-		if num-prev < curr-num {
-			fmt.Printf("Ближайшее число Фибоначчи: %d\n", prev)
-		} else {
-			fmt.Printf("Ближайшее число Фибоначчи: %d\n", curr)
-		}
-	}
+	console.Main()
 }
